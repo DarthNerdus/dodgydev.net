76eb02b4143f995f79b4f41edbe81d12285ba9f0
Jesse Read <darthnerdus@gmail.com>
2011-03-23 09:45:14 -0500

    Here we go!

---
+++ _posts/2009-09-22-using_git_to_manage_a_web_site.markdown
@@ -0,0 +1,9 @@
+---
+layout: link
+title: "Using Git to manage a web site"
+link: http://toroid.org/ams/git-website-howto
+---
+
+> The one-line summary: push into a remote repository that has a detached work tree, and a post-receive hook that runs `git checkout -f`.
+
+Great how-to for putting a website under the control of git and avoiding some of the issues that can pop up when using `git pull` from your server is not an option.
---
+++ convert.rb
@@ -0,0 +1,40 @@
+#!/usr/bin/env ruby
+
+files = Dir.glob("/Users/jread/Developer/blog/_blosxom/*.txt")
+
+for file in files
+  date_prefix = File.mtime(file).strftime("%Y-%m-%d")
+  lines = File.open(file, &:readlines)
+  posttitle = lines[0].gsub(/[a-zA-Z]*: /,'').chomp!
+  postname = posttitle.downcase.gsub(/[a-zA-Z]*: /,'').gsub(' ','_').gsub(/\W/,'')
+  postlayout = lines[0].gsub(/:.*/,'').downcase.chomp!
+  newpost = "/Users/jread/Developer/blog/_posts/#{date_prefix}-#{postname}.markdown"
+
+  if postlayout.eql? "text"
+    chop = 1
+    header = <<-END
+---
+layout: text
+title: "#{posttitle}"
+---
+
+END
+  else
+    chop = 3
+    header = <<-END
+---
+layout: #{postlayout}
+title: "#{posttitle}"
+#{postlayout}: #{lines[1].to_s.chomp!}
+---
+
+END
+  end
+
+  lines.shift(chop)
+  
+  File.open(newpost, 'w') do |f|
+    f << header
+    f << lines.join
+  end
+end
